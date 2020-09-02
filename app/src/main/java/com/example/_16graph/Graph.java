ccdfa610aa9a643630047857a63cc415a926fe79
guoyalong <guoyalong@gagogroup.com>
2020-09-02 13:25:47 +0800

    feat(*): 添加图的深度优先

--- app/src/main/java/com/example/_16graph/Graph.java
+++ app/src/main/java/com/example/_16graph/Graph.java
@@ -9,6 +9,8 @@ public class Graph {
     private List<String> vertexList; // 存储顶点集合
     private int[][] edges; // 存储图的对应的邻结矩阵
     private int numOfEdges; // 表示边的数目
+    // 定义一个boolean[]，记录某个结点是否被访问过
+    private boolean[] isVisited;
 
     public static void main(String[] args) {
         int n = 5; // 结点的个数
@@ -19,13 +21,14 @@ public class Graph {
         }
 
         // 添加边
-        graph.insertEdge(0,1,1);
-        graph.insertEdge(0,2,1);
-        graph.insertEdge(1,2,1);
-        graph.insertEdge(1,3,1);
-        graph.insertEdge(1,4,1);
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
 
         graph.showGraph();
+        graph.dfs();
 
     }
 
@@ -35,6 +38,61 @@ public class Graph {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+    /**
+     * 得到第一个邻接结点的下标 w
+     *
+     * @param index index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    // 根据前一个邻接结点的下标来获取下一个邻接结点
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    // 深度优先遍历算法
+    // i 第一次就是 0
+    private void dfs(boolean[] isVisited, int i) {
+        // 首先我们访问该节点，输出
+        System.out.print(getValueByIndex(i) + " -> ");
+        // 将结点设置为已经访问
+        isVisited[i] = true;
+
+        // 查找结点i的第一个邻接结点 w
+        int w = getFirstNeighbor(i);
+        while (w != -1) { // 说明有
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            // 如果w结点已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    // 对dfs 进行一个重载，遍历我们所有的结点，并进行dfs
+    public void dfs() {
+        // 遍历所有的结点进行dfs[]
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
     }
 
     // 图中常用的方法
